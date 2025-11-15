44adf2c774683b54c1f2fa9067d972ea5294dce7
hakoru1a <hakoru1a@gmail.com>
2025-11-15 15:27:31 +0700

    remove project base

--- src/Services/Base/Base.Domain/Entities/Order.cs
+++
@@ -1,8 +0,0 @@
-using Contracts.Domain;
-
-namespace Base.Domain.Entities;
-public class OrderBase : EntityAuditBase<long>
-{
-    public string OrderNumber { get; set; } = string.Empty;
-    public decimal TotalAmount { get; set; }
-}
